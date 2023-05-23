0b30d3ae43c2b1bddd36b215da9ca6e4df9d548d
rpallin0705 <rpallin0705@g.educaand.es>
2023-05-23 13:25:49 +0200

    serachbar en store

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -2,7 +2,19 @@
   <header>
     <div class="head">
       <img src="../assets/logo_texto.png">
-      <SearchItem></SearchItem>
+
+      <div class="box" v-show="$route.path === '/store'">
+        <input type="checkbox" id="check">
+        <div class="search-box">
+          <input type="text" placeholder="Type here...">
+          <label for="check" class="icon">
+            <span class="material-symbols-outlined">
+              search
+            </span>
+          </label>
+        </div>
+      </div>
+
       <nav>
         <input type="checkbox" id="checknav">
         <label for="checknav" class="checkbtn">
@@ -33,25 +45,12 @@
 
 
 <script>
-import SearchItem from './SearchItem.vue';
 
 export default {
   name: "MainHeader",
-  components: {
-    SearchItem,
 
-  }
 };
 
-
-document.addEventListener('scroll', () => {
-  const header = document.querySelector('header');
-  if (window.scrollY > 600) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-})
 </script>
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0");
@@ -179,5 +178,78 @@ a:hover {
   #checknav:checked~ul {
     left: 0;
   }
+}
+
+/*search */
+
+.material-symbols-outlined {
+  font-variation-settings:
+    'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 48
+}
+
+
+
+#check {
+  display: none;
+}
+
+.box {
+  max-width: 400px;
+  width: 100%;
+}
+
+.box .search-box {
+  position: relative;
+  height: 50px;
+  max-width: 50px;
+
+  margin-left: auto;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.5);
+  border-radius: 25px;
+  transition: all 0.3s ease;
+}
+
+#check:checked~.search-box {
+  max-width: 400px;
+
+
+}
+
+
+
+.search-box input {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  border-radius: 25px;
+  background: white;
+  outline: none;
+  border: none;
+  padding-left: 20px;
+  font-size: 18px;
+}
+
+.search-box .icon {
+  background: white;
+  position: absolute;
+  font-weight: bold;
+  right: -2px;
+  top: 0;
+  width: 50px;
+  height: 100%;
+  color: rgb(236, 108, 3);
+  text-align: center;
+  line-height: 65px;
+  border-radius: 25px;
+}
+
+#check:checked~.search-box .icon {
+  background: rgb(236, 108, 3);
+  color: white;
+  border-radius: 0 25px 25px 0;
+  width: 60px;
 }
 </style>
