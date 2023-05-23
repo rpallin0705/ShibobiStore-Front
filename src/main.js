c5d497527fa732dfc8dc57dae2c149e5693c034f
rpallin0705 <rpallin0705@g.educaand.es>
2023-05-23 13:40:23 +0200

    cambios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './components/HomePage.vue'
 //import StorePage from './components/StorePage.vue'
 import LogIn from './components/LogIn.vue'
@@ -22,7 +22,7 @@ const routes = [
 //Crear objeto rutas
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
